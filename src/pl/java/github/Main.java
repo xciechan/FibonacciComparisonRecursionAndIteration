d216164500c515fc887d013d3ea6c53742d2f0e1
Maciej Ciechanowski <ciechan92@gmail.com>
2022-05-29 13:25:55 +0200

    change long to BigInteger

--- src/pl/java/github/Main.java
+++ src/pl/java/github/Main.java
@@ -1,56 +1,65 @@
 package pl.java.github;
 
+import java.math.BigInteger;
+
 /**
  Created by xciechan
  on 2022-05-28
 
  The application displays n numbers of the fibonacci sequence.
  Compares the time of a recursive method and iterative method.
+
+      |  0                ,n=0
+ Fn = |  1                ,n=1
+      |  F(n-1) + F(n-2)  ,n>1
  */
 
 public class Main {
 
     public static void main(String[] args) {
         int rangeDisplayFibonacci = 45;
-        displayComparisonFibonacci(rangeDisplayFibonacci);
+        displayComparisonTimeFibonacci(rangeDisplayFibonacci);
 
     }
 
-    public static  void displayComparisonFibonacci(int range){
-        long startTime;
-        long stopTime;
+    public static  void displayComparisonTimeFibonacci(int range){
+        long startTime, stopTime;
+        long timeRecursion, timeIteration;
 
         for (int i=0; i<=range; i++) {
             startTime = System.nanoTime();
             System.out.print(i + "_r : " + fibonacciRecursion(i) + " : ");
             stopTime = System.nanoTime();
-            System.out.println((stopTime - startTime) + "ns (" + (stopTime - startTime) / 1000000 + "ms)");
+            timeRecursion = stopTime - startTime;
+            System.out.println(timeRecursion + "ns (" + timeRecursion / 1000000 + "ms)");
 
             startTime = System.nanoTime();
             System.out.print(i + "_i : " + fibonacciIteration(i) + " : ");
             stopTime = System.nanoTime();
-            System.out.println((stopTime - startTime) + "ns (" + (stopTime - startTime) / 1000000 + "ms)");
+            timeIteration = stopTime - startTime;
+            System.out.println(timeIteration + "ns (" + timeIteration / 1000000 + "ms)");
+
+            System.out.println("Time recursion = " + timeRecursion/timeIteration + "x Time iteration");
 
             System.out.println();
         }
-
     }
 
-    public static long fibonacciRecursion (int number){
-        if (number == 0) return 0;
-        if (number == 1) return 1;
+    public static BigInteger fibonacciRecursion (int number){
+        if (number == 0) return BigInteger.ZERO;
+        if (number == 1) return BigInteger.ONE;
 
-        return fibonacciRecursion(number - 1) + fibonacciRecursion(number - 2);
+        return fibonacciRecursion(number - 1).add(fibonacciRecursion(number - 2));
     }
 
-    public static long fibonacciIteration (int number){
-        long numberSmaller = 1;
-        long numberBigger = 0; // the number is smaller for the first number = 0
-        long numberAuxiliary;
+    public static BigInteger fibonacciIteration (int number){
+        BigInteger numberSmaller = BigInteger.ONE;
+        BigInteger numberBigger = BigInteger.ZERO; // the number is smaller for the first number = 0
+        BigInteger numberAuxiliary;
 
         for (int i = 0; i<number; i++){
             numberAuxiliary = numberBigger;
-            numberBigger = numberBigger + numberSmaller;
+            numberBigger = numberBigger.add(numberSmaller);
             numberSmaller = numberAuxiliary;
         }
 
